8579ca4f38c1bea3bc9d73077804f85e3eeb617a
Elliot Brazier <justashotoffire@gmail.com>
2020-01-16 14:28:31 -0600

    added comments, started inOrder arg

--- parse.py
+++ parse.py
@@ -1,18 +1,21 @@
-#take command line args
-#put in arr
-#loop thru text file
-#check if word contains each of the args
+#use "python parse.py" followed by any words or letters you want a word to contain
+# if looking for full words for example "python parse.py strike"
+# if looking for parts of words, "python parse.py st ike" 
+import sys 
 import argparse
-import sys
 if __name__ == "__main__":
-    args = []
-    goodWord = false
-    for x in (sys.argv[1:]):
-        args.append(x)
-    wordsFile = open("words.txt","r")
-    words = wordsFile.read().splitlines()
-    for word in words:
-        for arg in args:
-            if arg not in word:
-            
+    parser = argparse.ArgumentParser.add_argument('--inorder',  help="Only return words that have the arguments in order given")
+    args = [] #for storing arguments
+    goodWord = True #flag for later
+    for x in (sys.argv[1:]): #loop thru args, ignoring the first, which is the script name
+        args.append(x) # add to arg list
+    wordsFile = open("words.txt","r") 
+    words = wordsFile.read().splitlines() #set words to a list, each entry is separated by a line break
+    for word in words: #for every word
+        goodWord = True #resets the flag on each new word check
+        for arg in args: #for every argument
+            if arg not in word: #if the word doesnt contain the argument
+                goodWord = False #do not print the word
+        if goodWord:   
+            print(word)
     wordsFile.close()

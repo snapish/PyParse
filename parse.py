33a80628160b90d3bd64dfe66cd90cbf6720f1d2
Elliot Brazier <justashotoffire@gmail.com>
2020-01-21 13:48:43 -0600

    made progress on checking indicies

--- parse.py
+++ parse.py
@@ -4,7 +4,14 @@
 import sys 
 import argparse
 if __name__ == "__main__":
+    def check(list1, val): 
+        for x in list1: 
+            if val <= x: 
+                return False 
+        return True
+      
     args = [] #for storing arguments
+    indicies = [] #storing what index the string checker is at
     goodWord = True #flag for later
     for x in (sys.argv[1:]): #loop thru args, ignoring the first, which is the script name
         args.append(x) # add to arg list
@@ -12,12 +19,15 @@ if __name__ == "__main__":
     words = wordsFile.read().splitlines() #set words to a list, each entry is separated by a line break
     for word in words: #for every word
         goodWord = True #resets the flag on each new word check    
-        for arg in args: #for every argument
-            if arg not in word: #if the word doesnt contain the argument
+        for ind, arg in enumerate(args, start=0): #for every argument
+            if word.find(arg) != -1:
+                indicies.append(word.find(arg))
+            if arg not in word and check(indicies, word.find(arg)) : #if the word doesnt contain the argument
                 goodWord = False #do not print the word
-                
+        indicies = []  #clear the array
         if goodWord:        
             print(word)
     wordsFile.close()
 
      #if all(c in word for c in args):
+    # get index of where arg starts, when checking if agr is in word, AND index of arg is greater than all previous args

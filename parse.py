28131bf67e4ad027b477a86a1bdbde21e9baa3f1
Elliot Brazier <justashotoffire@gmail.com>
2020-01-22 14:56:46 -0600

    few more comments

--- parse.py
+++ parse.py
@@ -1,10 +1,11 @@
 #use "python parse.py" followed by any words or letters you want a word to contain
 # if looking for full words for example "python parse.py strike"
 # if looking for parts of words, "python parse.py st ike" 
+# you can set a length you want the words to be, add a number anywhere in your command
 import sys 
 import argparse
 if __name__ == "__main__":
-    def arraySortedOrNot(arr):   
+    def arraySortedOrNot(arr): #yoinked this bad boy off the internet  
         # Calculating length 
         n = len(arr) 
         # Array has one or no element or the 
@@ -21,27 +22,24 @@ if __name__ == "__main__":
     goodWord = True #flag for later
     for x in (sys.argv[1:]): #loop thru args, ignoring the first, which is the script name
         if x.isnumeric():
-            maxLength = int(x)
+            maxLength = int(x) 
         else:
-            args.append(x) # add to arg list
+            args.append(x) # add non-int arg list
 
     wordsFile = open("words.txt","r") 
     words = wordsFile.read().splitlines() #set words to a list, each entry is separated by a line break
     for word in words: #for every word
         goodWord = True #resets the flag on each new word check    
         for arg in args: #for every argument
-            if word.find(arg) != -1:
-                indicies.append(word.find(arg))
+            if word.find(arg) != -1: #if the arg is found in the word
+                indicies.append(word.find(arg)) #push the starting index to the indicies array
             if arg not in word: #if the word doesnt contain the argument
                 goodWord = False #do not print the word
-            if not arraySortedOrNot(indicies):
-                goodWord = False
+            if not arraySortedOrNot(indicies): # if the args were not found in order, their starting indicies were not in order
+                goodWord = False #dont print
         indicies = []  #clear the array
-        if goodWord and maxLength == 0:
+        if goodWord and maxLength == 0: #if no length was given in the command line
             print(word)
-        elif goodWord and maxLength > 0 and len(word) == maxLength: 
+        elif goodWord and maxLength > 0 and len(word) == maxLength: #if a length was given in the command line, print only words of that length
             print(word)
     wordsFile.close()
-
-     #if all(c in word for c in args):
-    # get index of where arg starts, when checking if agr is in word, AND index of arg is greater than all previous args
